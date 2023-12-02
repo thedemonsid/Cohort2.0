44c4208f593e22b841c0b067cf323b8067afbc06
thedemonsid <siddheshshrirame@gmail.com>
2023-12-03 04:11:07 +0530

    changes in script but not whole product will do last touchup tomorrow

--- week-0/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/script.js
+++ week-0/Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/script.js
@@ -8,15 +8,23 @@ var sounds = {
     yellow : new Audio('./sounds/yellow.mp3'),
     wrong : new Audio('./sounds/wrong.mp3')
 }
-for(let i=0;i<button.length;i++){
-    button[i].addEventListener('click',function(){
-
-        const color = button[i].getAttribute('id');
-        sequence.push(color);
-        
-        $(button[i]).fadeOut(100).fadeIn(100);
-
-        sounds[color].play();
-       // alert(`${color}`);
-    });
+function buttonClicked(){
+    const color = this.getAttribute('id');
+    $(this).fadeOut(100).fadeIn(100);
+    sounds[color].play();
 }
+for(let i=0;i<button.length;i++){
+    button[i].addEventListener('click',buttonClicked)
+};
+
+function gameStart(){
+       let i = Math.floor(Math.random()*4);
+       $(button[i]).fadeOut(100).fadeIn(100);
+       var color = button[i].getAttribute('id');
+       sounds[color].play();
+       sequence.push(i);
+}
+window.addEventListener('keydown', function(event) {
+    document.querySelector('h1').innerHTML='hmm';
+    gameStart();
+});
