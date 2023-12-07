4e17524d19af70993d593c6a7b069ae502056767
thedemonsid <siddheshshrirame@gmail.com>
2023-12-07 11:32:43 +0530

    basic map in array

---
+++ week-1/map.js
@@ -0,0 +1,14 @@
+let student = [
+    {
+        name:'siddhesh',
+        id:'907',
+        number : 23
+    },
+    {
+        name : 'anupama',
+        id : '987',
+        number: 223
+    }
+];
+let names = student.map((std)=>std.name+" hhi");
+console.log(names);
